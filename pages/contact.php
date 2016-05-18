002bba9646249635fb4a111842eacbc1cce43581
Angutivik Casper Rúnur Tausen Hansen <runurth@gmail.com>
2016-05-19 00:03:51 +0200

    Added subject field for contact form

--- pages/contact.php
+++ pages/contact.php
@@ -4,6 +4,7 @@
 
 		<input type="text" name="name" placeholder="Full name"><br>
 		<input type="email" name="email" placeholder="Email"><br>
+		<input type="text" name="subject" placeholder="Subject"><br>
 		<textarea name="message"></textarea><br>
 		<input type="submit" name="submit" value="Send">
 
