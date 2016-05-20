13941a861de7fa40034d9b0cc2fe30245c5fba44
Angutivik Casper Rúnur Tausen Hansen <runurth@gmail.com>
2016-05-20 13:13:08 +0200

    Moved around on some font thing so it takes fewer lines in the CSS. Added fonts on the input and textarea fields in forms. Changed font size in forms.

--- pages/contact.php
+++ pages/contact.php
@@ -5,7 +5,7 @@
 		<input type="text" name="name" placeholder="Full name"><br>
 		<input type="email" name="email" placeholder="Email"><br>
 		<input type="text" name="subject" placeholder="Subject"><br>
-		<textarea name="message"></textarea><br>
+		<textarea name="message" placeholder="Message"></textarea><br>
 		<input type="submit" name="submit" value="Send">
 
 	</form>
