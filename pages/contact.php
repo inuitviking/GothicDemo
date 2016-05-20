5972b87cf4277503a9baa18b29164674146c3969
Angutivik Casper Rúnur Tausen Hansen <runurth@gmail.com>
2016-05-20 16:00:11 +0200

    Made form look pretty

--- pages/contact.php
+++ pages/contact.php
@@ -1,11 +1,24 @@
 <main>
 
+	<!-- All forms should have this setup for the CSS to work properly -->
 	<form>
 
-		<input type="text" name="name" placeholder="Full name"><br>
-		<input type="email" name="email" placeholder="Email"><br>
-		<input type="text" name="subject" placeholder="Subject"><br>
-		<textarea name="message" placeholder="Message"></textarea><br>
+		<div class="fillout">
+			<label for="name">Full name</label>
+			<input type="text" name="name" id="name">
+		</div>
+		<div class="fillout">
+			<label for="email">Email</label>
+			<input type="email" name="email" id="email">
+		</div>
+		<div class="fillout">
+			<label for="subject">Subject</label>
+			<input type="text" name="subject" id="subject">
+		</div>
+		<div class="fillout">
+			<label for="message">Message</label>
+			<textarea name="message" id="message"></textarea>
+		</div>
 		<input type="submit" name="submit" value="Send">
 
 	</form>
