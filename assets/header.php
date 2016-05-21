e8fdbe44393cd431de96038fe538f986f1b7313d
Angutivik Casper Rúnur Tausen Hansen <runurth@gmail.com>
2016-05-21 17:47:52 +0200

    changed title

--- assets/header.php
+++ assets/header.php
@@ -11,7 +11,7 @@
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<link rel="icon" type="icon" href="img/main/favicon.ico">
 
-		<title><?=ucfirst(str_replace('_', ' ', $_GET['page'])) ?></title>
+		<title><?=ucfirst(str_replace('_', ' ', $_GET['page']))." - Gothic Demo" ?></title>
 
 	</head>
 
