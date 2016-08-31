90da1e5e5f89db64eb150873026e2bdfefdb4bdc
Angutivik Casper Rúnur Tausen Hansen <Angutivik Casper Rnur Tausen Hansen>
2016-08-31 16:47:01 +0200

    Added dummy content to "history"

--- pages/history.php
+++ pages/history.php
@@ -1,5 +1,16 @@
 <main>
 
+	<h1>History Archive</h1>
 
+	<section>
+
+		<article>
+
+			<img src="http://static.memrise.com/uploads/course_photos/3146044000150629230451.jpg">
+			<p>Text about a thing in something</p>
+
+		</article>
+
+	</section>
 
 </main>
