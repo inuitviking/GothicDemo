e2e6ba211479c7f15f2b45c39459a506384c68a0
Angutivik Casper Rúnur Tausen Hansen <Angutivik Casper Rnur Tausen Hansen>
2016-08-31 16:40:25 +0200

    Added stuff in "about"

--- pages/about.php
+++ pages/about.php
@@ -1,5 +1,20 @@
 <main>
 
-	
+	<h1>About Gothic Demo</h1>
+
+	<hr>
+
+	<section>
+
+		<article>
+
+			<p>This is a demo site that I am working on.<br>
+			It has to use gothic fonts and seem somewhat old schoolish, but not be resource heavy, so a ton of pictures is not an option.<br>
+			It also has to be mobile friendly and easy to use for both normal functioning people and disabled people.</p>
+
+
+		</article>
+
+	<section>
 
 </main>
